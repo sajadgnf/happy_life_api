02daf83f60422fc9f77321d27f87f9e8d314f94a
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-03-11 11:35:16 +0330

    happy-life-api

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
-
 const productsRoutes = require("./routes/products-routes")
-
 const app = express()
+let port = process.env.PORT || 8000
 
 app.use('/', productsRoutes)
 
-app.listen(process.env.PORT || 8000)
+app.listen(port)
