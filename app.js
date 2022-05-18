78ba7f275c47d84295cb19150cbca898b458f8d4
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-05-18 14:50:38 +0430

    add compression

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const productsRoutes = require("./routes/product-routes")
 const app = express()
 const cors = require('cors')
+const compression = require('compression')
 let port = process.env.PORT || 8000
+
+app.use(
+    compression({
+        level: 6,
+        threshold: 10 * 1000,
+        filter: (req, res) => {
+            if (req.headers['x-no-compression']) {
+                return false
+            }
+            return compression.filter(req, res)
+        }
+    })
+)
 
 app.use(
     cors({
